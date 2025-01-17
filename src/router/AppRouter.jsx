e7c1cc1d093be3e59a11feb7deb3d29e19875632
Lrev47 <lrev4766@gmail.com>
2025-01-17 15:07:51 -0600

    Update AppRouter.jsx

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,31 @@
+// src/router/AppRouter.jsx
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Homepage from '../pages/homepage/homepage.jsx';
+import FlashcardList from '../components/FlashcardList/FlashcardList.jsx';
+import FlashcardDetailPage from '../pages/FlashcardDetailPage/FlashcardDetailPage.jsx';
+import TopicPage from '../pages/TopicPage/TopicPage.jsx';
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 1) Homepage at "/" */}
+        <Route path="/" element={<Homepage />} />
+
+        {/* 2) Flashcard List at "/cards" */}
+        <Route path="/cards" element={<FlashcardList />} />
+
+        {/* 3) Flashcard Detail at "/cards/:cardId" */}
+        <Route path="/cards/:cardId" element={<FlashcardDetailPage />} />
+
+        {/* 4) Topic Page at "/topics" */}
+        <Route path="/topics" element={<TopicPage />} />
+        <Route path="/topics/:topicId" element={<TopicPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
