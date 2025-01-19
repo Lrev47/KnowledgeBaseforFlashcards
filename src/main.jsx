0d9b8aa1493c917eae8f2f95429fdff073aa9d42
Lrev47 <lrev4766@gmail.com>
2025-01-18 19:01:51 -0600

    update for syntax

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store.js'; // Named export from store.js
 import App from './App.jsx';              // We'll define a default export in App.jsx
+import "./style/index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
